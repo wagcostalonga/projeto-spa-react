85522e5d0b9cd76185f35cc7170adfde79a7c245
wagcostalonga <costalongawagner@gmail.com>
2020-02-05 12:42:11 +0000

    first commit

---
+++ spa/src/pages/Main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Title } from './styles';
+
+function Main() {
+  return (
+    <Container>
+      <Title>Main</Title>
+    </Container>
+  );
+}
+
+export default Main;
